bb6f5bb2d49028ad59f61807e657a2f77739b055
AdriSolid <you@example.com>
2018-12-02 14:23:55 +0100

    Updates

--- precache-manifest.5eda297e50dd9915c6eed5609fcfe8e8.js
+++ precache-manifest.5eda297e50dd9915c6eed5609fcfe8e8.js
@@ -4,15 +4,15 @@ self.__precacheManifest = [
     "url": "/ReactExamples/static/js/runtime~main.b5a0d3cf.js"
   },
   {
-    "revision": "6d677f4bc02fb407cac3",
-    "url": "/ReactExamples/static/js/main.6d677f4b.chunk.js"
+    "revision": "647f5ba6b66110e5a693",
+    "url": "/ReactExamples/static/js/main.647f5ba6.chunk.js"
   },
   {
     "revision": "633503f29ef19fa84640",
     "url": "/ReactExamples/static/js/1.633503f2.chunk.js"
   },
   {
-    "revision": "6d677f4bc02fb407cac3",
+    "revision": "647f5ba6b66110e5a693",
     "url": "/ReactExamples/static/css/main.8592ca4b.chunk.css"
   },
   {
@@ -20,7 +20,7 @@ self.__precacheManifest = [
     "url": "/ReactExamples/static/css/1.18cc70e5.chunk.css"
   },
   {
-    "revision": "08fb5054d1d909a5de57c8394d40bdff",
+    "revision": "3baa1496a31ab8b947844abdb5a5680c",
     "url": "/ReactExamples/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/ReactExamples/precache-manifest.384da96ddc9865c4ad35d039169660b2.js"
+  "/ReactExamples/precache-manifest.5eda297e50dd9915c6eed5609fcfe8e8.js"
 );
 
 workbox.clientsClaim();
